03e042e651afe2d3e462e31e2b6c125872ecede6
Kirill Dmitruk <Dmitruk.ks@mail.ru>
2024-07-04 14:56:11 +0300

    added docstrings

--- users/views.py
+++ users/views.py
@@ -7,32 +7,42 @@ from users.serializers import UserSerializer
 
 
 class UserCreateAPIView(generics.CreateAPIView):
+    """Создание нового пользователя."""
+
     serializer_class = UserSerializer
     queryset = User.objects.all()
     permission_classes = (
         AllowAny,
     )  # Открывает доступ для неавторизованных пользователей
 
-    def perform_create(self, serializer):  # один из вариантов создания user (более простой)
+    def perform_create(self, serializer):
         user = serializer.save(is_active=True)
         user.set_password(user.password)
         user.save()
 
 
 class UserListAPIView(generics.ListAPIView):
+    """Получение списка всех пользователей."""
+
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 
 class UserRetrieveAPIView(generics.RetrieveAPIView):
+    """Получение информации о конкретном пользователе."""
+
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 
 class UserUpdateAPIView(generics.UpdateAPIView):
+    """Изменение информации о пользователе."""
+
     serializer_class = UserSerializer
     queryset = User.objects.all()
 
 
 class UserDestroyAPIView(generics.DestroyAPIView):
+    """Удаление пользователя."""
+
     queryset = User.objects.all()
